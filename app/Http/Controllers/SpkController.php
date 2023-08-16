e32bd3caaabf8a9878f25cdb1366038596d667c1
UPT TIK Unsri <ka.tik@unsri.ac.id>
2023-08-16 07:01:55 +0700

    spk

--- app/Http/Controllers/SpkController.php
+++ app/Http/Controllers/SpkController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Spk;
 use App\Models\Vendor;
 use App\Models\Customer;
+use App\Models\TemplateSpk;
 use Illuminate\Http\Request;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Carbon\Carbon;
@@ -104,14 +105,40 @@ class SpkController extends Controller
     public function spk_doc(Spk $spk)
     {
         $data = $spk;
-        $customer = Customer::all();
+        $template = TemplateSpk::orderBy('halaman', 'asc')->get();
+
+        $hk_mip = $data->vendor->vendor_bayar->where('pembayaran', $data->pembayaran)->where('customer_id', 1)->first()->harga_kesepakatan;
+        $hk_bp = $data->vendor->vendor_bayar->where('pembayaran', $data->pembayaran)->where('customer_id', 2)->first()->harga_kesepakatan;
+
+        // $customer = Customer::all();
         // make $data->tanggal_indo from $data->tanggal
         $data->tanggal_indo = Carbon::parse($data->tanggal)->isoFormat('D MMMM Y');
         // make $data->tahun from $data->tanggal
         $data->tahun = Carbon::parse($data->tanggal)->format('Y');
-        $pdf = PDF::loadView('dokumen.spk.doc', [
-            'data' => $data,
-            'customer' => $customer,
+
+        foreach ($template as $t) {
+            $t->content = str_replace('{{$tahun}}', $data->tahun, $t->content);
+            $t->content = str_replace('{{$tanggal}}', $data->tanggal_indo, $t->content);
+            $t->content = str_replace('{{$nomor}}', sprintf("%03d", $data->nomor), $t->content);
+            $t->content = str_replace('{{$singkatan}}', $data->nama_singkatan, $t->content);
+            $t->content = str_replace('{{$nama_vendor}}', $data->vendor->nama, $t->content);
+            $t->content = str_replace('{{$jabatan_vendor}}', $data->vendor->jabatan, $t->content);
+            $t->content = str_replace('{{$perusahaan_vendor}}', $data->vendor->perusahaan ? $data->vendor->perusahaan : '-', $t->content);
+            $t->content = str_replace('{{$perusahaan}}', $data->vendor->perusahaan ? $data->vendor->perusahaan : '', $t->content);
+            $t->content = str_replace('{{$alamat_vendor}}', $data->vendor->alamat, $t->content);
+            $t->content = str_replace('{{$no_hp_vendor}}', $data->vendor->no_hp, $t->content);
+            $t->content = str_replace('{{$email_vendor}}', $data->vendor->email, $t->content);
+            $t->content = str_replace('{{$pembayaran}}', strtoupper($data->pembayaran), $t->content);
+            $t->content = str_replace('{{$harga_kesepakatan_mip}}', number_format($hk_mip, 0, ',', '.'), $t->content);
+            $t->content = str_replace('{{$harga_kesepakatan_bp}}', number_format($hk_bp, 0, ',', '.'), $t->content);
+            $t->content = str_replace('{{$bank}}', $data->vendor->bank, $t->content);
+            $t->content = str_replace('{{$no_rekening}}', $data->vendor->no_rekening, $t->content);
+            $t->content = str_replace('{{$nama_rekening}}', $data->vendor->nama_rekening, $t->content);
+        }
+
+        $pdf = Pdf::loadView('dokumen.template.template-spk.doc', [
+            // 'data' => $data,
+            'template' => $template,
         ]);
         return $pdf->stream($data->nomor.' - SPK '.$data->vendor->perusahaan.' - '.$data->vendor->nama.'.pdf');
     }
