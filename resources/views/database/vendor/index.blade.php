c4f4031c10724fee77e3ed34de5c2cf419adadff
UPT TIK Unsri <ka.tik@unsri.ac.id>
2023-10-19 17:59:41 +0700

    update

--- resources/views/database/vendor/index.blade.php
+++ resources/views/database/vendor/index.blade.php
@@ -71,7 +71,7 @@
                           <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-l" role="document">
                               <div class="modal-content">
                                   <div class="modal-header">
-                                      <h5 class="modal-title" id="modalTitleId">Modal title</h5>
+                                      <h5 class="modal-title" id="modalTitleId">Kesepakatan Uang Jalan</h5>
                                           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                   </div>
                                   <div class="modal-body">
