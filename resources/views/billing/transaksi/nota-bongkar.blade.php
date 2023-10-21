894888fe0cfe2d1f5adbbb18dcc3c4acb8c097e3
UPT TIK Unsri <ka.tik@unsri.ac.id>
2023-10-21 17:41:06 +0700

    up

--- resources/views/billing/transaksi/nota-bongkar.blade.php
+++ resources/views/billing/transaksi/nota-bongkar.blade.php
@@ -156,7 +156,32 @@
                 <td class="text-center align-middle">{{$d->nota_muat}}</td>
                 <td class="text-center align-middle">{{$d->tonase}}</td>
                 <td class="text-center align-middle">
-                    <button class="btn btn-warning btn-block">Void</button>
+                    <button class="btn btn-warning btn-block" type="button" data-bs-toggle="modal" data-bs-target="#modalVoid-{{$d->id}}">Void</button>
+
+                    <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
+                    <div class="modal fade" id="modalVoid-{{$d->id}}" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="modalTitleId">Masukan Password</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <form action="{{route('transaksi.void-masuk', $d)}}" method="post">
+                                    @csrf
+                                <div class="modal-body">
+                                    <input type="password" class="form-control" id="password" name="password"
+                                            placeholder="Password" aria-label="Password" aria-describedby="password"
+                                            required>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="submit" class="btn btn-primary">Save</button>
+                                </div>
+                            </form>
+                            </div>
+                        </div>
+                    </div>
+
                 </td>
             </tr>
             <script>
