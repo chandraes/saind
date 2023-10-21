3d25c88f9c9a9109690d0f589ef5563455a9d000
UPT TIK Unsri <ka.tik@unsri.ac.id>
2023-10-21 20:19:22 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -150,7 +150,6 @@ Route::group(['middleware' => ['auth']], function() {
     Route::post('transaksi/void-masuk/{transaksi}', [App\Http\Controllers\TransaksiController::class, 'void'])->name('transaksi.void-masuk');
     Route::post('transaksi/void/{transaksi}', [App\Http\Controllers\TransaksiController::class, 'void_store'])->name('transaksi.void.store');
     Route::post('transaksi/back/{transaksi}', [App\Http\Controllers\TransaksiController::class, 'back'])->name('transaksi.back');
-)
 
     Route::resource('bbm-storing', App\Http\Controllers\BbmStoringController::class);
 
