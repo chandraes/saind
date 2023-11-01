4f53350de321911e7f84a03b13d3da90afeda038
UPT TIK Unsri <ka.tik@unsri.ac.id>
2023-11-01 18:12:09 +0700

    up

--- resources/views/database/direksi/create.blade.php
+++ resources/views/database/direksi/create.blade.php
@@ -279,18 +279,21 @@
             $('#gaji_pokok').maskMoney({
                 thousands: '.',
                 decimal: ',',
-                precision: 0
+                precision: 0,
+                allowZero: true
             });
             $('#tunjangan_jabatan').maskMoney({
                 thousands: '.',
                 decimal: ',',
-                precision: 0
+                precision: 0,
+                allowZero: true
             });
 
             $('#tunjangan_keluarga').maskMoney({
                 thousands: '.',
                 decimal: ',',
-                precision: 0
+                precision: 0,
+                allowZero: true
             });
         });
 
--- resources/views/database/direksi/edit.blade.php
+++ resources/views/database/direksi/edit.blade.php
@@ -280,17 +280,20 @@
             $('#gaji_pokok').maskMoney({
                 thousands: '.',
                 decimal: ',',
-                precision: 0
+                precision: 0,
+                allowZero: true
             });
             $('#tunjangan_jabatan').maskMoney({
                 thousands: '.',
                 decimal: ',',
-                precision: 0
+                precision: 0,
+                allowZero: true
             });
             $('#tunjangan_keluarga').maskMoney({
                 thousands: '.',
                 decimal: ',',
-                precision: 0
+                precision: 0,
+                allowZero: true
             });
         });
 
