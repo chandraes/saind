a7895f50597cb42376fa9856f41abecc6c37d224
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-11-02 17:22:22 +0700

    add new user roles to the users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('username')->unique();
-            $table->enum('role', ['admin', 'user', 'vendor'])->default('user');
+            $table->enum('role', ['su','admin', 'user', 'vendor', 'vendor-operational', 'customer', 'operasional'])->default('user');
             $table->string('name');
             $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
